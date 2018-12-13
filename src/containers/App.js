c06b6aba07a31428a96c080ed4fc73a0f31621f3
Piotr Nastaj <piotr@nastaj.net>
2018-12-13 12:37:24 +0100

    Fix errors, Add presentation component Title

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [
+                {
+                    text: "a",
+                    id: 1231231231234
+                },
+                {
+                    text: "b",
+                    id: 567856857567567
+                }],
+            title: 'Webpack loaders training'
         };
     }
-    addTodo(val){
+    addTodo(val) {
         const todo = {
             text: val,
             id: uuid.v4(),
         };
         const data = [...this.state.data, todo];
-        this.setState({data});
-	}
-	
-	removeTodo(id) {
-		const remainder = this.state.data.filter(todo => todo.id !== id);
-		this.setState({data: remainder});
-	}
+        this.setState({ data });
+    }
 
-	render() {
-		return (
-			<div className={style.TodoApp}>
-				Tutaj pojawią się komponenty naszej aplikacji.
-			</div>
-		);
-	}
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({ data: remainder });
+    }
+
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                <Title title={this.state.title} numberOfTasks={this.state.data.length} />
+            </div>
+        );
+    }
 }
 
 export default App;
